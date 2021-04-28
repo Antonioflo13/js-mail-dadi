21bf158dee6efae87d8bde15dd1e90086763ea2b
Antonioflo13 <floreantonio@hotmail.it>
2021-04-28 16:58:31 +0200

    check email and secret code

--- party-invite/js/script.js
+++ party-invite/js/script.js
@@ -1,19 +1,33 @@
 // 1 - Mail
 // Chiedi all'utente la sua email,
 var email = prompt("Ciao qual'è la tua E-mail?");
-console.log(email);
 
 // controlla che sia nella lista di chi può accedere,
-var invited = ["floreantonio@htomail.it", "antonioflore@hotmail.it", "matteo.r@gmail.com"];
-console.log(invited);
+var invited = ["floreantonio@hotmail.it", "antonioflore@hotmail.it", "matteo.r@gmail.com"];
 
+var secretCode = "weeknd"
+
+var inList = false;
 for (var i = 0; i < invited.length; i++) {
-
-    if ( invited [i] == email ) {
-        console.log("Ecco il tuo invito!");
-    }
     
+    if ( invited[i] == email ) {
+        inList = true;
+    }
 }
-console.log("Non sei stato invitato, inserisci il codice segreto!");
 
+if (inList == true) {
+    console.log("Ecco il tuo invito!");
+} else if (inList == false) {
+    var code = prompt("Inserisci il codice segreto per accreditarti!")
+} 
+
+checkCode = true;
+if (secretCode !== code) {
+    checkCode = false;
+    console.log("Il codice è errato non puoi accreditarti!");
+} else {
+    invited.push(email);
+    console.log(invited);
+    console.log("Perfetto, ti sei accreditato!");
+}
 // stampa un messaggio appropriato sull'esito del controllo.
